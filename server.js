151d838992ea748627966ad4c1e69c90bdb21ad1
Piotr Kamecki <piotrkamecki8@gmail.com>
2023-03-04 21:39:32 +0100

    endpoints assemble task

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+
+const testimonials = require('./routes/testimonials.routes.js');
+const concerts = require('./routes/concerts.routes.js');
+const seats = require('./routes/seats.routes.js');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+app.use('/api', testimonials);
+app.use('/api', concerts);
+app.use('/api', seats);
+
+  app.use((req, res) => {
+    res.status(404).send('404 not found...');
+  });
+
+app.listen(8000, () => {
+  console.log('Server is running on port: 8000');
+});
