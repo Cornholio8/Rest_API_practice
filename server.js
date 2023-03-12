63b74873b2e5773cefd6f33593657ffc845be3b9
Piotr Kamecki <piotrkamecki8@gmail.com>
2023-03-13 00:35:20 +0100

    prepare for replit

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const testimonials = require('./routes/testimonials.routes.js');
 const concerts = require('./routes/concerts.routes.js');
@@ -11,14 +12,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/client/build')));
+
 app.use('/api', testimonials);
 app.use('/api', concerts);
 app.use('/api', seats);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
 
   app.use((req, res) => {
     res.status(404).send('404 not found...');
   });
 
-app.listen(8000, () => {
-  console.log('Server is running on port: 8000');
-});
+  app.listen(process.env.PORT || 8000, () => {
+    console.log('Server is running on port: 8000');
+  });
